cac8b481c4648cd7a9f899585cad3e6720e58a26
Atakan Erhan <atakanbayil@gmail.com>
2023-12-12 15:24:03 +0300

    first commit

---
+++ Plane2/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Plane2.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // Other actions like Register, Logout, etc.
+    }
+}
